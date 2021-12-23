982a6c1adca77d73e6735641f0697808867c29ed
Jared Bruni <jaredbruni@gmail.com>
2021-12-22 19:59:53 -0800

    output text formatting

--- source/text2c/bin2c.pl
+++ source/text2c/bin2c.pl
@@ -6,7 +6,7 @@ sub convertStream {
         read(STDIN, $buffer, 256);
         my @bytes = split("", $buffer);
         for(my $i = 0; $i < scalar @bytes; $i++) {
-            printf("0x%x, ", ord($bytes[$i]));
+            printf("0x%x,", ord($bytes[$i]));
         }
     }
     print "0};\n";
