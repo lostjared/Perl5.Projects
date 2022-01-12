076da63b181a0b8cfa62e21f98a51de99fd75e4e
Jared Bruni <jaredbruni@gmail.com>
2022-01-11 16:08:21 -0800

    added script

---
+++ source/sort_images/sort_images.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+my $dir = shift @ARGV;
+my $src = shift @ARGV;
+opendir $d, $dir or die("Could not open directory");
+my @f = readdir $d;
+closedir $d;
+my %list = ();
+foreach $fi (@f) {
+    my $pos = index $fi, $src;
+    if($pos != -1) {
+        my $pos2 = index $fi, "_", $pos;
+        if($pos2 != -1) {
+            my $pos3 = index $fi, "_", $pos2+1;
+            if($pos3 != -1) {
+                my $r = substr($fi, $pos+1, $pos3-$pos-1);
+                my $t = substr($r, $ti, length ($r));
+                
+                if ($r =~ m/(\d+).(\d+).(\d+)_(\d+).(\d+).(\d+)/) {
+                    
+                    $s = $2 . "-" . $3 . "-" . $1 . " " . $4 . ":" . $5 . ":" . $6;
+                    $list{$s} = $fi;
+                }
+            }
+        }
+    }
+}
+foreach my $i (sort keys %list) {
+    print $list{$i} . "\n";
+    #print $i . "\n";
+}
